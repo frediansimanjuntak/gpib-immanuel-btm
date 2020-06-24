54ee80efcd0248dd78b5f152ea028088805bc6e7
fredian <fredian.pc@gmail.com>
2020-06-24 14:43:33 +0700

    change unique phone number

--- database/migrations/2020_06_23_161419_create_user_details_table.php
+++ database/migrations/2020_06_23_161419_create_user_details_table.php
@@ -17,7 +17,7 @@ class CreateUserDetailsTable extends Migration
             $table->id();
             $table->string('full_name')->nullable();
             $table->string('full_address')->nullable();
-            $table->string('phone_number')->nullable();
+            $table->string('phone_number')->unique()->nullable();
             $table->string('birth_date')->nullable();
             $table->string('birth_place')->nullable();
             $table->string('family_status')->nullable();
